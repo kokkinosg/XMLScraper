4a022e5d96edb9ab209806319935f55290b9971b
Georgios Kokkinos <gk769@bath.ac.uk>
2025-03-31 12:16:00 +0100

    Created FileLocator class

---
+++ FileLocator.java
@@ -0,0 +1,21 @@
+// This class is responsible for examining a given directory and returning all files of the specified extension
+
+public class FileLocator {
+
+    //#region Variables
+
+    //#endregion
+
+    //#region Constructor
+    public FileLocator(){
+
+    }
+
+    //#endregion 
+
+    //#region Methods
+
+
+    //#region
+    
+}
